df44e513da9d6a4e6110e0d525a5143e4f1db2c2
vanhopld98 <vanhopld98@gmail.com>
2021-08-04 15:09:25 +0700

    THuc Hanh

---
+++ src/main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = c', please enter constants");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("c: ");
+        double c = scanner.nextDouble();
+
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.println("Equation pass with x = "+answer);
+        }else {
+            System.out.println("No solution!");
+        }
+    }
+}
